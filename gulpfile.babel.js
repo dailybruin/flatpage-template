0c85934028a72f625a19782af1f64ab4ffa78e0d
Nathan Smith <nathan@nathansmith.io>
2018-02-23 18:14:03 -0800

    Changes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,37 +1,24 @@
 import gulp from 'gulp';
-import nunjucksRender from 'gulp-nunjucks-render';
-
-// Images
-import imagemin from 'gulp-imagemin';
 
 // HTML
-import htmlmin from 'gulp-htmlmin';
+import nunjucksRender from 'gulp-nunjucks-render';
+
+// HTML
+// import htmlmin from 'gulp-htmlmin';
 
 // Styling related packages
-import sass from 'gulp-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import minifyCSS from 'gulp-csso';
+// import sass from 'gulp-sass';
+// import postcss from 'gulp-postcss';
+// import autoprefixer from 'autoprefixer';
+// import minifyCSS from 'gulp-csso';
 
-// JavaScript
-import webpack from 'webpack-stream';
-import uglify from 'gulp-uglify';
-
-import sourcemaps from 'gulp-sourcemaps';
+// Browsersync
 import bs from 'browser-sync';
-import del from 'del';
-
 const browserSync = bs.create();
 
-gulp.task('images:dev', () => gulp.src('src/img/*').pipe(gulp.dest('dev/img')));
-
-gulp.task('images:prod', () =>
-  gulp.src('src/img/*').pipe(imagemin()).pipe(gulp.dest('prod/img'))
-);
-
-gulp.task('styles:dev', () =>
+gulp.task('styles', () =>
   gulp
-    .src('./src/scss/**/*.scss')
+    .src('./src/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(
       sass({
@@ -44,60 +31,7 @@ gulp.task('styles:dev', () =>
     .pipe(browserSync.stream())
 );
 
-gulp.task('styles:prod', () =>
-  gulp
-    .src('./src/scss/**/*.scss')
-    .pipe(
-      sass({
-        outputStyle: 'expanded',
-      }).on('error', sass.logError)
-    )
-    .pipe(postcss([autoprefixer()]))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('./prod/css'))
-    .pipe(browserSync.stream())
-);
-
-const webpackConfig = {
-  module: {
-    rules: [
-      {
-        test: /.js$/,
-        use: [
-          {
-            loader: 'babel-loader',
-          },
-        ],
-      },
-    ],
-  },
-  output: {
-    filename: 'bundle.js',
-  },
-};
-
-gulp.task('scripts:dev', () =>
-  gulp
-    .src('src/js/index.js')
-    .pipe(sourcemaps.init())
-    .pipe(webpack(webpackConfig))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dev/js'))
-);
-
-gulp.task('scripts:prod', () =>
-  gulp
-    .src('src/js/index.js')
-    .pipe(webpack(webpackConfig))
-    .pipe(
-      uglify().on('error', e => {
-        console.log(e);
-      })
-    )
-    .pipe(gulp.dest('prod/js'))
-);
-
-gulp.task('html:dev', () =>
+gulp.task('html', () =>
   gulp
     .src('src/*.{njk,html}')
     .pipe(
@@ -108,46 +42,20 @@ gulp.task('html:dev', () =>
     .pipe(gulp.dest('dev/'))
 );
 
-gulp.task('html:prod', () =>
-  gulp
-    .src('src/*.{njk,html}')
-    .pipe(
-      nunjucksRender({
-        path: ['src/'],
-      })
-    )
-    .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('prod/'))
-);
-
-gulp.task(
-  'development',
-  ['html:dev', 'styles:dev', 'scripts:dev', 'images:dev'],
-  () => {
-    browserSync.init({
-      server: {
-        baseDir: './dev',
-        serveStaticOptions: {
-          extensions: ['html'],
-        },
+gulp.task('development', ['html'], () => {
+  browserSync.init({
+    server: {
+      baseDir: './dev',
+      serveStaticOptions: {
+        extensions: ['html'],
       },
-    });
+    },
+  });
 
-    gulp.watch('./src/img/**/*', ['images:dev']);
-    gulp.watch('./src/scss/**/*.scss', ['styles:dev']);
-    gulp.watch('./src/js/**/*.js', ['scripts:dev']);
-    gulp
-      .watch('src/**/*.{njk,html}', ['html:dev'])
-      .on('change', browserSync.reload);
-  }
-);
-
-gulp.task('production', [
-  'html:prod',
-  'styles:prod',
-  'scripts:prod',
-  'images:prod',
-]);
+  gulp
+    .watch('src/**/*.{njk,html}', ['html:dev'])
+    .on('change', browserSync.reload);
+});
 
 gulp.task('clean', () => del(['dev/', 'prod/']));
 gulp.task('default', ['development']);
